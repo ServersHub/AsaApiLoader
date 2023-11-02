72ee0978c0cdf731baea0960d7b732e2b23fabb1
Lethalinjectionx <lethalinjectionx@gmail.com>
2023-11-02 12:13:29 -0400

    Updated loader to fix default windows terminal color display.

--- AsaApiLoader/AsaApiLoader.cpp
+++ AsaApiLoader/AsaApiLoader.cpp
@@ -11,50 +11,56 @@ namespace fs = std::filesystem;
 
 int reject(HANDLE process, const std::string& message = "Critical Loading Error!")
 {
-    logger::error("{}", message);
+	logger::error("{}", message);
 
 	for (int i = 3; i > 0; --i)
-    {
-        using namespace std::chrono_literals;
-        logger::info("Closing in {}", i);
-        std::this_thread::sleep_for(1000ms);
-    }
+	{
+		using namespace std::chrono_literals;
+		logger::info("Closing in {}", i);
+		std::this_thread::sleep_for(1000ms);
+	}
 
 	if (process)
-        TerminateProcess(process, 0);
+		TerminateProcess(process, 0);
 
-    return 0;
+	return 0;
 }
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
 
-    fs::path server = loader::findExe();
-    fs::path dll = loader::findDll();
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD dwMode = 0;
+	GetConsoleMode(hOut, &dwMode);
+	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	SetConsoleMode(hOut, dwMode);
 
-    if (!std::filesystem::exists(server))
-        return reject(nullptr, "Server Missing!");
+	fs::path server = loader::findExe();
+	fs::path dll = loader::findDll();
 
-    if (!std::filesystem::exists(dll))
-        return reject(nullptr, "Api Missing!");
+	if (!std::filesystem::exists(server))
+		return reject(nullptr, "Server Missing!");
 
-    logger::success("Detected Server: {}", server.filename().string());
-    logger::success("Detected Api: {}", dll.filename().string());
+	if (!std::filesystem::exists(dll))
+		return reject(nullptr, "Api Missing!");
 
-    STARTUPINFOW startupInfo = { 0 };
-    PROCESS_INFORMATION	procInfo = { nullptr };
-    startupInfo.cb = sizeof(startupInfo);
-    DWORD createFlags = CREATE_SUSPENDED;
+	logger::success("Detected Server: {}", server.filename().string());
+	logger::success("Detected Api: {}", dll.filename().string());
 
-    CreateProcess(server.c_str(), GetCommandLine(), nullptr, nullptr, FALSE, createFlags, nullptr, nullptr, &startupInfo, &procInfo);
+	STARTUPINFOW startupInfo = { 0 };
+	PROCESS_INFORMATION	procInfo = { nullptr };
+	startupInfo.cb = sizeof(startupInfo);
+	DWORD createFlags = CREATE_SUSPENDED;
 
-    const auto result = inject(procInfo.dwProcessId, dll);
+	CreateProcess(server.c_str(), GetCommandLine(), nullptr, nullptr, FALSE, createFlags, nullptr, nullptr, &startupInfo, &procInfo);
 
-    if (!result)
-        return reject(procInfo.hProcess);
+	const auto result = inject(procInfo.dwProcessId, dll);
 
-    ResumeThread(procInfo.hThread);
+	if (!result)
+		return reject(procInfo.hProcess);
 
-    WaitForSingleObject(procInfo.hProcess, INFINITE);
-    return 0;
+	ResumeThread(procInfo.hThread);
+
+	WaitForSingleObject(procInfo.hProcess, INFINITE);
+	return 0;
 }
