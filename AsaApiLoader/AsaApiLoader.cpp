a9070da2e82a78bc7d9b68550f8721ef8898de4d
Lethalinjectionx <lethalinjectionx@gmail.com>
2023-11-02 14:42:37 -0400

    removed tabbed spacing changes

--- AsaApiLoader/AsaApiLoader.cpp
+++ AsaApiLoader/AsaApiLoader.cpp
@@ -11,56 +11,56 @@ namespace fs = std::filesystem;
 
 int reject(HANDLE process, const std::string& message = "Critical Loading Error!")
 {
-	logger::error("{}", message);
+    logger::error("{}", message);
 
-	for (int i = 3; i > 0; --i)
-	{
-		using namespace std::chrono_literals;
-		logger::info("Closing in {}", i);
-		std::this_thread::sleep_for(1000ms);
-	}
+    for (int i = 3; i > 0; --i)
+    {
+        using namespace std::chrono_literals;
+        logger::info("Closing in {}", i);
+        std::this_thread::sleep_for(1000ms);
+    }
 
-	if (process)
-		TerminateProcess(process, 0);
+    if (process)
+        TerminateProcess(process, 0);
 
-	return 0;
+    return 0;
 }
 
 int main() {
-	SetConsoleOutputCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
 
-	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	DWORD dwMode = 0;
-	GetConsoleMode(hOut, &dwMode);
-	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-	SetConsoleMode(hOut, dwMode);
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
 
-	fs::path server = loader::findExe();
-	fs::path dll = loader::findDll();
+    fs::path server = loader::findExe();
+    fs::path dll = loader::findDll();
 
-	if (!std::filesystem::exists(server))
-		return reject(nullptr, "Server Missing!");
+    if (!std::filesystem::exists(server))
+        return reject(nullptr, "Server Missing!");
 
-	if (!std::filesystem::exists(dll))
-		return reject(nullptr, "Api Missing!");
+    if (!std::filesystem::exists(dll))
+        return reject(nullptr, "Api Missing!");
 
-	logger::success("Detected Server: {}", server.filename().string());
-	logger::success("Detected Api: {}", dll.filename().string());
+    logger::success("Detected Server: {}", server.filename().string());
+    logger::success("Detected Api: {}", dll.filename().string());
 
-	STARTUPINFOW startupInfo = { 0 };
-	PROCESS_INFORMATION	procInfo = { nullptr };
-	startupInfo.cb = sizeof(startupInfo);
-	DWORD createFlags = CREATE_SUSPENDED;
+    STARTUPINFOW startupInfo = { 0 };
+    PROCESS_INFORMATION	procInfo = { nullptr };
+    startupInfo.cb = sizeof(startupInfo);
+    DWORD createFlags = CREATE_SUSPENDED;
 
-	CreateProcess(server.c_str(), GetCommandLine(), nullptr, nullptr, FALSE, createFlags, nullptr, nullptr, &startupInfo, &procInfo);
+    CreateProcess(server.c_str(), GetCommandLine(), nullptr, nullptr, FALSE, createFlags, nullptr, nullptr, &startupInfo, &procInfo);
 
-	const auto result = inject(procInfo.dwProcessId, dll);
+    const auto result = inject(procInfo.dwProcessId, dll);
 
-	if (!result)
-		return reject(procInfo.hProcess);
+    if (!result)
+        return reject(procInfo.hProcess);
 
-	ResumeThread(procInfo.hThread);
+    ResumeThread(procInfo.hThread);
 
-	WaitForSingleObject(procInfo.hProcess, INFINITE);
-	return 0;
+    WaitForSingleObject(procInfo.hProcess, INFINITE);
+    return 0;
 }
